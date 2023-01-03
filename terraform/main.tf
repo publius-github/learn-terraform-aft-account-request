74a38c69f611f5a164273bc5efa66cc2f429aad2
Your Name <you@example.com>
2023-01-03 13:57:38 +0100

    a-kachan account

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,3 +84,31 @@ module "sandboxlucius" {
   account_customizations_name = "sandbox"
 }
 
+
+module "sandboxakachan" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "workfos@yandex.ru"
+    AccountName               = "a.kachan-sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "a.kachan@godeltech.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
